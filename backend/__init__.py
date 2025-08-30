6e8aff573b4852801d70e128c5b656013a20cb42
juancitoozmarichal07-glitch <juancitoozmarichal.07@gmail.com>
2025-08-30 14:35:51 -0300

    Delete backend/__init__.py

--- backend/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py (VERSIÓN FINAL Y DEFINITIVA)
-
-# El punto (.) significa "desde esta misma carpeta".
-# Le decimos a Python que el archivo "guardian.py" es parte de este paquete.
-from .guardian import Guardian
